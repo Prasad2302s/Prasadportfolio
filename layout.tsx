640900e4c11b5c864241cd4063a716fb8e27fe65
Prasad2302s <itzprasadss@gmail.com>
2025-07-29 00:01:15 +0530

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,49 @@
+
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Pacifico } from "next/font/google";
+import "./globals.css";
+
+const pacifico = Pacifico({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-pacifico',
+})
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Prasad S Sawant - Designer × Editor",
+  description: "Creative designer and editor specializing in video editing, UI design, and 3D animation. Bringing ideas to life through innovative visual storytelling.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <style>{`
+          html {
+            scroll-behavior: smooth;
+          }
+        `}</style>
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${pacifico.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
